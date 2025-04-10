b9365d4924aa66c2fee77a7d40306f154394e47d
Shivam0069 <shivamsinghchauhan7000@gmail.com>
2025-04-11 02:10:12 +0530

    Add Photo Picker functionality

--- client/src/components/common/PhotoPicker.jsx
+++ client/src/components/common/PhotoPicker.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-
-function PhotoPicker() {
-  return <div>PhotoPicker</div>;
+import ReactDOM from "react-dom";
+function PhotoPicker({ onChange }) {
+  const component = (
+    <input type="file" onChange={onChange} hidden id="photo_picker" />
+  );
+  return ReactDOM.createPortal(
+    component,
+    document.getElementById("photo_picker_element")
+  );
 }
 
 export default PhotoPicker;
