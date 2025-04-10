91886a5ac0461a950bc44e752bda242bef0a60d6
Shivam0069 <shivamsinghchauhan7000@gmail.com>
2025-04-11 02:09:32 +0530

    Add Context Menu

--- client/src/components/common/ContextMenu.jsx
+++ client/src/components/common/ContextMenu.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
-function ContextMenu() {
-  return <div>ContextMenu</div>;
+function ContextMenu({ options, coordinates, contextMenu, setContextMenu }) {
+  const contextMenuRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (event.target.id !== "context_opener") {
+        if (
+          contextMenuRef.current &&
+          !contextMenuRef.current.contains(event.target)
+        ) {
+          setContextMenu(false);
+        }
+      }
+    };
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  const handleClick = (e, callback) => {
+    e.stopPropagation();
+    setContextMenu(false);
+    callback();
+  };
+  return (
+    <div
+      className={`bg-dropdown-background fixed  z-[100]  shadow-xl `}
+      ref={contextMenuRef}
+      style={{
+        top: coordinates.y,
+        left: coordinates.x,
+      }}
+    >
+      <ul>
+        {options.map(({ name, callback }) => (
+          <li
+            key={name}
+            className="px-5 py-2 hover:bg-background-default-hover cursor-pointer "
+            onClick={(e) => {
+              handleClick(e, callback);
+            }}
+          >
+            <span className="text-white ">{name}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default ContextMenu;
