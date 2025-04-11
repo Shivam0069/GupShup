99c34bb4248f8c7e034a784327694d3a9f602dab
Shivam0069 <shivamsinghchauhan7000@gmail.com>
2025-04-11 22:41:32 +0530

    Chat SideBar

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ChatList from "./Chatlist/ChatList";
 import Empty from "./Empty";
+import { onAuthStateChanged } from "firebase/auth";
+import { firebaseAuth } from "@/utils/FirebaseConfig";
+import { useStateProvider } from "@/context/StateContext";
+import axios from "axios";
+import { CHECK_USER_ROUTE } from "@/utils/ApiRoutes";
+import { useRouter } from "next/router";
+import { reducerCases } from "@/context/constants";
 
 function Main() {
+  const [redirectLogin, setRedirectLogin] = useState(false);
+  const router = useRouter();
+  const [{ userInfo }, dispatch] = useStateProvider();
+
+  useEffect(() => {
+    if (redirectLogin) router.push("/login");
+  }, [redirectLogin]);
+
+  onAuthStateChanged(firebaseAuth, async (user) => {
+    if (!user) setRedirectLogin(true);
+    if (!userInfo && user?.email) {
+      const { data } = await axios.post(CHECK_USER_ROUTE, {
+        email: user.email,
+      });
+
+      if (!data?.success) {
+        router.push("/login");
+      }
+      if (data?.data) {
+        const { id, name, email, profilePicture: profileImage } = data.data;
+        dispatch({
+          type: reducerCases.SET_USER_INFO,
+          userInfo: {
+            id,
+            name,
+            email,
+            profileImage,
+            status: data?.success,
+          },
+        });
+      }
+    }
+  });
   return (
     <>
       <div className="grid grid-cols-main h-screen w-screen max-h-screen max-w-full overflow-hidden">
